217e807c41cf3539695c15d960cf8f9c1bc05a30
Taylor Monacelli <taylormonacelli@gmail.com>
2024-02-11 16:54:16 -0800

    ok

--- src/anyglobal/__init__.py
+++ src/anyglobal/__init__.py
@@ -1,2 +1,24 @@
+import argparse
+
+
 def hello():
     return "Hello from anyglobal!"
+
+
+def main():
+    parser = argparse.ArgumentParser(description="Description of your program")
+    parser.add_argument("--path", help="Specify the path")
+    args = parser.parse_args()
+
+    # Access the specified path using args.path
+    if args.path:
+        print("Path specified:", args.path)
+    else:
+        print("No path specified.")
+
+    # Call hello function
+    print(hello())
+
+
+if __name__ == "__main__":
+    main()
